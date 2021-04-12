f1fc48b62ef1bbae36860635f7bd3093dc743fb5
imwbh <wbh2031@163.com>
2021-04-12 15:47:52 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -26,7 +26,7 @@ module.exports = {
         //repoLabel: 'Repo',
         // 以下为可选的编辑链接选项
         // 假如你的文档仓库和项目本身不在一个仓库：
-        //docsRepo: 'vuejs/vuepress',
+        docsRepo: 'imwbh/type',
         // 假如文档不是放在仓库的根目录下：
         docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
